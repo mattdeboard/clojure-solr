eb5b40df49ad1b7839c04f2b341d0601ac6bf8b6
Cory Giles <cory.b.giles@gmail.com>
2011-07-22 18:32:19 -0500

    Personal clojars version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-solr "0.3.0-SNAPSHOT"
+(defproject org.clojars.gilesc/clojure-solr "0.3.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.apache.solr/solr-solrj "3.3.0"]])
