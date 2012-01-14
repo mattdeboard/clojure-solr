84c153fc0181e846df8509f02cc2226e9c144ccf
Matt DeBoard <matt.deboard@gmail.com>
2012-01-14 02:55:44 -0500

    Update repository for Clojure 1.3 & Solr 3.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-solr "0.3.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [org.apache.solr/solr-solrj "3.3.0"]])
+(defproject clojure-solr "0.4.0-SNAPSHOT"
+  :description "Clojure 1.3 bindings for Apache Solr 3.5."
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.apache.solr/solr-solrj "3.5.0"]])
