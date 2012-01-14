a233a559b3f20e2cb4ba26d09897483278022abb
Matt DeBoard <matt.deboard@gmail.com>
2012-01-14 03:08:25 -0500

    Add group namespace to project definition for Clojars.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-solr "0.4.0-SNAPSHOT"
+(defproject org.clojars.mattdeboard/clojure-solr "0.4.0-SNAPSHOT"
   :description "Clojure 1.3 bindings for Apache Solr 3.5."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.apache.solr/solr-solrj "3.5.0"]])
