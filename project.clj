d6c4382f0a7b704f8ced61580a2467e95ecf60aa
Matt DeBoard <matt.deboard@gmail.com>
2012-02-27 11:13:49 -0500

    Add lein-clojars to dev deps.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject org.clojars.mattdeboard/clojure-solr "0.4.0-SNAPSHOT"
+(defproject org.clojars.mattdeboard/clojure-solr "0.4.0"
   :description "Clojure 1.3 bindings for Apache Solr 3.5."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.apache.solr/solr-solrj "3.5.0"]])
+                 [org.apache.solr/solr-solrj "3.5.0"]]
+  :dev-dependencies [[lein-clojars "0.6.0"]])
